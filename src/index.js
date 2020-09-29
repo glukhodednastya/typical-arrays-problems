fbbdcf72e8304acde275e54a5331527dd2e7e294
Anastasia Glukhoded <glukhodednastya@gmail.com>
2020-09-29 21:06:29 +0300

    feat: change src file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array && array.length != 0) {
+    var minimum = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < minimum) {
+            minimum = array[i];
+        }
+    }
+    return minimum;
+  } else {
+    return 0;
+  };
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array && array.length != 0) {
+    var maximum = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > maximum) {
+            maximum = array[i];
+        }
+    }
+    return maximum;
+  } else {
+    return 0;
+  };
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array && array.length != 0) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum/array.length;
+  } else {
+    return 0;
+  };
+};
